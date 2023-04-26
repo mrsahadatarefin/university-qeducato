b7c86b0f1e2aed52aeba1161f0657537fa39b823
Al Shaharia <ifty682002@gmail.com>
2023-04-27 01:59:08 +0600

    pc banner concept is done

--- components/Shared/Navbar.tsx
+++ components/Shared/Navbar.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [closeNavbar, setCloseNavbar] = useState(false);
   return (
     <div>
       {/* navbar header */}
-      <div className="bg-[#125875] text-black flex justify-between lg:pr-[100px]">
+      <div className="bg-[#125875] text-black xl:flex justify-between lg:pr-[100px] hidden">
         <div className="flex items-center bg-[#FF7350] text-white lg:px-[100px] py-7">
           <h2>Follow Us:- </h2>
           <div className="flex gap-4 px-2 text-2xl">
@@ -50,12 +51,12 @@ const Navbar = () => {
       </div>
       {/* navbar */}
       <div className="bg-white text-black">
-        <div className="mx-[100px] flex justify-between items-center">
+        <div className="xl:mx-[100px] mx-5 my-5 lg:my-0 flex justify-between items-center">
           <Link href={"/"}>
             <img src="/web-logo.png" alt="website logo" className="w-[260px]" />
           </Link>
-          <div className="text-xl flex">
-            <div className="flex gap-4">
+          <div className="text-xl lg:flex hidden items-center">
+            <div className="flex gap-4 mr-2">
               <Link
                 className="nav my-4 hover:text-[#FF7350] duration-300"
                 href={"/"}
@@ -93,14 +94,84 @@ const Navbar = () => {
                 Contact
               </Link>
             </div>
-            <div className="flex items-center">
+            <div className="xl:flex hidden items-center">
               <Link
-                className="bg-[#FF7350] hover:bg-[#125875] duration-300 text-white py-4 px-6 font-semibold"
+                className="bg-[#FF7350] hover:bg-[#125875] duration-300 text-white py-5 px-6 font-semibold"
                 href={"/admission-open"}
               >
                 ADMISSION OPEN
               </Link>
             </div>
+          </div>
+          <div className="dropdown dropdown-bottom dropdown-end lg:hidden">
+            <label
+              onClick={() => setCloseNavbar(true)}
+              tabIndex={0}
+              className="btn m-1"
+            >
+              <i className="fa-solid fa-bars"></i>
+            </label>
+            {closeNavbar && (
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <Link
+                    className="nav my-4 bg-transparent hover:text-[#FF7350] duration-300"
+                    href={"/"}
+                    onClick={() => setCloseNavbar(false)}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="nav my-4 bg-transparent hover:text-[#FF7350] duration-300"
+                    href={"/about"}
+                    onClick={() => setCloseNavbar(false)}
+                  >
+                    About Us
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="nav my-4 bg-transparent hover:text-[#FF7350] duration-300"
+                    href={"/courses"}
+                    onClick={() => setCloseNavbar(false)}
+                  >
+                    Courses
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="nav my-4 bg-transparent hover:text-[#FF7350] duration-300"
+                    href={"/pages"}
+                    onClick={() => setCloseNavbar(false)}
+                  >
+                    Pages
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="nav my-4 bg-transparent hover:text-[#FF7350] duration-300"
+                    href={"/blog"}
+                    onClick={() => setCloseNavbar(false)}
+                  >
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="nav my-4 bg-transparent hover:text-[#FF7350] duration-300"
+                    href={"/contact"}
+                    onClick={() => setCloseNavbar(false)}
+                  >
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            )}
           </div>
         </div>
       </div>
