3de70dc35646d195b8729ccb2533003449b049a4
Foysal Hossain <foysalx4@gmail.com>
2023-04-26 09:14:51 +0600

    first_commit

---
+++ components/features/Feautures.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styles from '../../styles/feauters.module.css'
+
+const Feautures = () => {
+    return (
+        <div>
+            
+            <div className={`${styles.features_main} flex flex-wrap py-12`}>
+                <div className={`${styles.features_text} p-1 lg:p-9`} >
+                    <div className="p-4 mx-0 lg:mx-6">
+                    <h1 className='text-5xl font-bold'>Our Best Features</h1>
+                    <p className='mt-5'>Special wedding garments are often worn, and the ceremony is sometimes followed by a wedding reception. Music, poetry.</p>
+                    </div>
+                    <div className={`${styles.logo_text} flex flex-wrap gap-5 py-4 p-2 mx-0 lg:mx-6`}>
+                        <div className={styles.s}>
+                         <img src="https://wpdemo.zcubethemes.com/qeducato/wp-content/uploads/2023/03/fea-icon01.png" alt="" />    
+                        </div>
+                              
+                        <div className="">               
+                        <h2>Skilled Teachers</h2>
+                        <p className='py-2'>Special wedding garments are often worn, and the ceremony is sttimes <br /> followed by a wedding reception. Music, poetry, prayers, or readings from.</p>
+                        </div> 
+                    </div>
+                    <div className={`${styles.logo_text} flex flex-wrap gap-5 py-4 p-2 lg:mx-6`}>
+                    <div className={styles.s}>
+                        <img src="https://wpdemo.zcubethemes.com/qeducato/wp-content/uploads/2023/03/fea-icon02.png" alt="" /> 
+                        </div>
+                        <div className="">                     
+                        <h2>Affordable Courses</h2>
+                        <p className='py-2'>Special wedding garments are often worn, and the ceremony is sttimes <br /> followed by a wedding reception. Music, poetry, prayers, or readings from.</p>
+                        </div> 
+                    </div>
+                    <div className={`${styles.logo_text} flex flex-wrap gap-5 py-4 p-2 lg:mx-6`}>
+                    <div className={styles.s}>
+                        <img src="https://wpdemo.zcubethemes.com/qeducato/wp-content/uploads/2023/03/fea-icon03.png" alt="" />  
+                        </div>
+                        <div className="">               
+                        <h2>Efficient & Flexible</h2>
+                        <p className='py-2'>Special wedding garments are often worn, and the ceremony is sttimes <br /> followed by a wedding reception. Music, poetry, prayers, or readings from.</p>
+                        </div>  
+                    </div>
+                </div>
+                <div className={`${styles.features_img}`}>
+                    <img src="https://wpdemo.zcubethemes.com/qeducato/wp-content/uploads/2023/03/steps-img.png" alt="" />
+                                   
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Feautures;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Feautures from "@/components/features/Feautures";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main className="">
       
+      <Feautures />
     </main>
   );
 }
