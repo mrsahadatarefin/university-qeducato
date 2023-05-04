37657be3ded778f1e6d73590d84d7194e969c683
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-05-04 23:08:58 +0600

    all project done

--- components/Events/Events.js
+++ components/Events/Events.js
@@ -1,48 +1,47 @@
-import React, { useEffect } from 'react';
-import styles from '../../styles/features/events.module.css'
-import { GetServerSideProps } from 'next';
-import { FaGraduationCap } from 'react-icons/fa'
-import { BsArrowRight } from 'react-icons/bs'
+import React from 'react';
+import Link from 'next/link';
+import {BsArrowRight} from 'react-icons/bs'
 
-const Events = ({ data }) => {
-    console.log(data);
 
+
+
+
+
+
+const EventPages = ({data} ) => {
+    //console.log(data);
+   
     return (
         <div>
-
-            <div className={`${styles.events_main} py-8`}>
-                <h4 className={`${styles.our_event} flex text-center py-2 justify-center font-semibold`}> <span className='mt-2 mx-2'><FaGraduationCap /></span>  Our Events</h4>
-
-                <h2 className={`${styles.upcoming} text-center py-2`}>Upcoming Events</h2>
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                    {
-                        data.events.map(event => <div key={event.id} className="events mt-6 py-2 p-4 ">
-                            <div className={`${styles.event} card`}>
-                                <img src={event.image} alt="" />
-                                <div className={`${styles.date}`}>
-                                    <h5>{event.date.slice(0, 2)} </h5>
-                                    <p>March 2023</p>
-                                </div>
-                                <div className={`${styles.card_info} p-4`}>
-                                    <h2>{event.name}</h2>
-                                    <p className=''>{event.info.slice(0, 110)}..</p>
-                                    <div className={`flex py-2 mt-2`}>
-                                        <p className=''>{event.date} </p> <span className='mx-2 mt-1'><BsArrowRight /></span>
-                                        <p className={`${styles.address} lg:mx-0 mx-0 font-semibold`}>{event.address}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>)
-                    }
-                </div>
-
+            <div className='hero  text-center   h-[600npx] '  >
+                <h1 className='text-6xl font-bold text-black  mt-20 '>Our Event</h1>
+               
             </div>
+            
+            <div className={`max-w-screen-xl mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7  py-8`}>
+           {
+            data.events.map(event  => <div key={event.id} className="events mt-6 py-2 p-4 ">
+               <Link href={`/events/${event.id}`}> <div className={` card`}>
+                    <img src={event.image} alt="" />
+                    <div className='w-28 p-2 -mt-12 bg-[#ff7350] text white text-center'>
+                        <h5 className='font-bold text-2xl text-white'>{event.date.slice(0,2)} </h5>
+                        <p className='text-white text-xs'>March 2023</p>
+                    </div>
+                    <div className='p-4  hover:bg-[#d1eefa] '>
+                    <h2 className='text-3xl font-semibold py-2'>{event.name}</h2>
+                    <p className=''>{event.info.slice(0,110)}..</p>
+                    <div className={`flex py-2 mt-2`}>
+                        <p className=''>{event.date} </p> <span className='mx-2 mt-1'><BsArrowRight /></span>
+                        <p className={` lg:mx-0 mx-0 font-semibold`}>{event.address}</p>
+                    </div>
+                    </div>
+                </div>
+                 </Link>
+            </div>)
+           }
+           </div>
+           
         </div>
     );
 };
-
-
-export default Events;
-
-
-
+export default EventPages;
--- components/features/Scholarship.tsx
+++ components/features/Scholarship.tsx
@@ -1,31 +1,22 @@
 import React from 'react';
-import styles from '../../styles/features/events.module.css'
+
 import {BsArrowRight} from 'react-icons/bs'
-import {FaGraduationCap} from 'react-icons/fa'
+// import {FaGraduationCap} from 'react-icons/fa'
 
 const Scholarship = () => {
     return (
         <div className='py-12 '>
-           <div className={`${styles.scholar} py-10 p-2 flex flex-wrap justify-around`}>
+           <div className={`bg-[#125875]  py-10 p-2 flex flex-wrap items-center justify-around`}>
             <div className="">
-                <h1>Scholarship Programs</h1>
-                <p>At Estuidar University, we prepare you to launch your career by providing a supportive, creative, and <br /> professional environment from which to learn practical skills and build a network of industry contacts.</p>
+                <h1 className='text-white text-5xl font-bold pb-4 '>Scholarship Programs</h1>
+                <p className='text-gray-300 text-xl '>At Estuidar University, we prepare you to launch your career by providing a supportive, creative, and <br /> professional environment from which to learn practical skills and build a network of industry contacts.</p>
             </div>
-            <button className='btn btn-md'>Financial Aid <span className='mx-2 font-semibold text-2xl'><BsArrowRight /></span>  </button>
+           <div>
+           <button className='btn bg-[#ff7350]  '>Financial Aid <span className='mx-2 font-semibold text-2xl'><BsArrowRight /></span>  </button>
+           </div>
            </div>
 
-           <div className={`${styles.school} flex flex-wrap gap-5 justify-evenly py-16`}>
-            <div className="school_img lg:p-0 p-2">
-                <img src="https://wpdemo.zcubethemes.com/qeducato/wp-content/uploads/2023/03/about_img_04.png" alt="" />
-            </div>
-            <div className={`${styles.school_text} mt-12 lg:p-0 p-2`}>
-                <h4 className='text-2xl font-semibold flex'> <span className='mx-2 text-1xl mt-1'><FaGraduationCap /></span> About Our High School</h4>
-                <h1 className='text-4xl font-bold py-4'>We Are High School Since <br /> 10 Years Experience</h1>
-                <p className='text-gray-500 text-lg py-1'>Our community is being called to reimagine the future. As the only university where a renowned design school comes together with premier colleges, we are making learning more relevant and transformational.</p> 
-                <p className='py-5'>We are proud to offer top ranige in employment services such and asser payroll and benefits administrato managemen and asistance with global business range ployment employer readings from religious texts or literature are also commonly inc compliance.</p>
-                <button className='btn btn-md border-none'>Read more <span className='mx-2 text-2xl ' ><BsArrowRight /></span></button>
-            </div>
-           </div>
+           
         </div>
     );
 };
